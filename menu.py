2495b6bff03d7037b50a2e472b78ffa2f501d432
alexgicimo <gakurualexgicimo@gmail.com>
2024-08-22 07:05:06 +0000

    get the menu

--- menu.py
+++ menu.py
@@ -7,14 +7,20 @@ class MenuItem:
             "coffee": coffee,
         }
         self.cost = cost
+    def __str__(self):
+        return self.name
 
 class Menu:
     def __init__(self):
-        self.menu = [
+        self.menu_items = [
                     MenuItem(name= "latte", milk= 150, water = 200, coffee = 24, cost = 2.5),
                     MenuItem(name = "espresso", water = 50, coffee = 18, cost = 1.5, milk = 0 ),
                     MenuItem(name = "cappuccino", milk = 100, water = 250, coffee = 24, cost = 3),
         ]
-    def get_menu(self):
-        return "".join(self.menu["name"])
-    
+
+    def get_menu_items(self):
+        menu = []
+        for item in self.menu_items:
+            menu.append(item.name)
+        return "/".join(menu)
+        

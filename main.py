1f7b47c5f2a211f237018afcd7085abdbfe66eeb
alexgicimo <gakurualexgicimo@gmail.com>
2024-08-22 08:39:27 +0000

    main

--- main.py
+++ main.py
@@ -9,16 +9,21 @@ def main():
     is_on = True
     while is_on:
         drink = input(f"Menu item : {menu.get_menu_items()}: ").lower()
-
+        
         if drink == "end":
             break
         elif drink == "report":
             print(coffee_machine.get_resources())
             print(f"Profit {coins.get_profit()}")
         else:
+            drink = menu.get_drink(drink)
             make_coffee, missing_resources = coffee_machine.are_resources_enough(drink)
             if make_coffee:
-                coins.are_coins_enough(drink)
+                coins_enough =coins.are_coins_enough(drink)
+                if coins_enough:
+                    coffee_machine.deduct_resources(drink)
+                    print(f"Here is your {drink.name} Enjoy!")
+
 
 if __name__ == "__main__":
     main()

a5767cb361953f8df48e0ba12253138678bcdf3a
alexgicimo <gakurualexgicimo@gmail.com>
2024-08-22 05:44:58 +0000

    coins class

--- coins_machine.py
+++ coins_machine.py
@@ -1,6 +1,20 @@
 class Coins:
     profit = 0
     COINS = {
-        "quarters":0.25,
-        ""
+        "quarter":0.25,
+        "dime":0.10,
+        "nickel":0.05,
+        "penny":0.01,
     }
+    def ask_for_coins(self):
+        for coin in self.COINS:
+            coins_total += int(input(f"Please insert number of {coin}: ")) * self.COINS[coin]
+        return coins_total
+    def are_coins_enough(self, drink):
+        coins =self.ask_for_coins()
+        
+        if coins >= drink["cost"]:
+            change =coins - drink["cost"]
+            print(f"Here is {change} in change")
+            return True
+        
